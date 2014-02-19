97b6cacd2fa97a64ec240248e1e4d6821a7ab435
ahmed-ibrahem <blood_line1986@yahoo.com>
2014-02-19 22:13:00 +0200

    alterAccountConnection

--- DentistSoftware/DentistSoftware/Models/IdentityModels.cs
+++ DentistSoftware/DentistSoftware/Models/IdentityModels.cs
@@ -10,7 +10,7 @@ namespace DentistSoftware.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection")
+            : base("DentistEntities")
         {
         }
     }
