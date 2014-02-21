d2fa1b6cd15a851a6ab2ba8ef0ba18afffe3c6ef
batman86 <Marchosni86@gmail.com>
2014-02-21 02:09:31 +0200

    sccope

--- DentistSoftware/DentistSoftware/DependencyResolution/StructureMapDependencyScope.cs
+++ DentistSoftware/DentistSoftware/DependencyResolution/StructureMapDependencyScope.cs
@@ -130,5 +130,49 @@ namespace DentistSoftware.DependencyResolution
         }
 
         #endregion
+
+       #region Methods
+
+        /// <summary>
+        /// When implemented by inheriting classes, this method will do the actual work of
+        ///        resolving all the requested service instances.
+        /// </summary>
+        /// <param name="serviceType">
+        /// Type of service requested.
+        /// </param>
+        /// <returns>
+        /// Sequence of service instance objects.
+        /// </returns>
+        //protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        //{
+        //    return this.Container.GetAllInstances(serviceType).Cast<object>();
+        //}
+
+        /// <summary>
+        /// When implemented by inheriting classes, this method will do the actual work of resolving
+        ///        the requested service instance.
+        /// </summary>
+        /// <param name="serviceType">
+        /// Type of instance requested.
+        /// </param>
+        /// <param name="key">
+        /// Name of registered service you want. May be null.
+        /// </param>
+        /// <returns>
+        /// The requested service instance.
+        /// </returns>
+        //protected override object DoGetInstance(Type serviceType, string key)
+        //{
+        //    if (string.IsNullOrEmpty(key))
+        //    {
+        //        return serviceType.IsAbstract || serviceType.IsInterface
+        //                   ? this.Container.TryGetInstance(serviceType)
+        //                   : this.Container.GetInstance(serviceType);
+        //    }
+
+        //    return this.Container.GetInstance(serviceType, key);
+        //}
+
+        #endregion
     }
 }
